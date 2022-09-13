d82c668e24decbbca0624d64a866a3037b0f2f18
Vladimir Below <laitvladimir@yandex.ru>
2022-09-13 21:01:59 +0300

    U: Доработана главная страница
    Signed-off-by: Vladimir Below <laitvladimir@yandex.ru>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from "next";
 import { PrismaClient, User } from "@prisma/client" 
 import { ReactNode, useEffect } from "react";
-import bghome from '../public/bg-home.jpg'
+
 import { Section } from "../components/home/section";
 
-export async function getServerSideProps(context: any) {
+export async function getServerSideProps(_: any) {
 	const prisma = new PrismaClient()
 	const data = await prisma.user.findMany()
 	return { props: { data } }
@@ -16,11 +16,25 @@ type HomeProps = {
 }
 
 const Home: NextPage<HomeProps> = ({ data }) => {
-let imageUrl = 'https://expertnov.ru/800/600/https/www.duodesign.co.uk/images/blog/cms7-cropped.pagespeed.1556696139.jpg/rs-2560x10000a.jpg'
+
 	useEffect(() => {
-		console.log( data )
-		console.log(bghome)
+		window.addEventListener('scroll', ( event ) => {
+				let headerBlock = document.querySelector('header')
+				document.querySelector('.arrow-buttom').style.opacity = 1 - (window.scrollY / 500)
+
+				if ( window.scrollY > ( window.innerHeight / 1.5 ) ) {
+					if ( !headerBlock!.classList.contains('bg-neutral-900') ) {
+						headerBlock!.classList.add('bg-neutral-900')
+					}
+				} 
+				else {
+					if ( headerBlock!.classList.contains('bg-neutral-900') ) {
+						headerBlock!.classList.remove('bg-neutral-900')
+					}
+				}
+		})
 	})
+	let imageUrl = 'https://expertnov.ru/800/600/https/www.duodesign.co.uk/images/blog/cms7-cropped.pagespeed.1556696139.jpg/rs-2560x10000a.jpg'
 	return (
 	<>
 		<div 
@@ -32,7 +46,7 @@ let imageUrl = 'https://expertnov.ru/800/600/https/www.duodesign.co.uk/images/bl
 				<h3 className="text-2xl font-thin">Если ты хочешь к нам то заходи к нам в группу, мы всем рады</h3>
 			</div>
 			<div className="absolute bg-dark-500 w-screen h-screen opacity-70%"></div>
-			<div className="z-100 absolute bottom-20">
+			<div className="z-100 absolute bottom-20 arrow-buttom">
 				<svg xmlns="http://www.w3.org/2000/svg" width="35" height="20.012" viewBox="0 0 35 20.012">
 					<path id="Icon_ionic-ios-arrow-down" data-name="Icon ionic-ios-arrow-down" d="M23.694,25.226,36.928,11.981a2.491,2.491,0,0,1,3.533,0,2.522,2.522,0,0,1,0,3.543l-15,15.006a2.5,2.5,0,0,1-3.449.073L6.917,15.535a2.5,2.5,0,0,1,3.533-3.543Z" transform="translate(-6.188 -11.246)" fill="#f5f5f5"/>
 				</svg>
@@ -42,7 +56,9 @@ let imageUrl = 'https://expertnov.ru/800/600/https/www.duodesign.co.uk/images/bl
 		<main className="w-7/12 mx-auto py-50px">
 
 	<Section title="О нас" >
-	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto culpa commodi minima libero repudiandae! Possimus odit iusto eaque nisi, consequatur id tempora amet laboriosam. Sequi ducimus magnam rerum asperiores eos!</p>
+		<p className="text-lg font-light">
+		Если коротко. И лень читать что ниже. То вот главная мысль нашей движухи. Мы организуем движение. Где если тянет программист 80% стартапа, он и будет получать с него 80% дохода. Создаём свои координаты справедливости. В нашем движении всё просто, если нас будет 1000, а работать будут только двое. Значит всю прибыль будут получать только двое. Хоть вставляй в наше движение всю нашу землю, получать деньги будет только горстка, которая работает.
+		</p>
 	</Section>
 
 	<Section title="Участники" >
